7145656fb9c4420a1506fa650ce39950f3c4de21
poohmean <49038656+poohmean@users.noreply.github.com>
2020-04-14 20:50:55 +0700

    Test

---
+++ src/components/Homepage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <h1 class="head">ProG reviews</h1>
+    <div class="ss">Samsung</div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Homepage"
+};
+</script>
+    
+
+
+<style scope lang = "scss">
+.head {
+  margin: 0;
+  text-align: left;
+  font-size: 45px;
+  font-weight: 400;
+  color: #ffffff;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  background-color: black;
+  padding-left: 0.5cm;
+}
+.ss {
+  color: #ffffff;
+  text-align: left;
+  padding-left: 5cm;
+  border-bottom: 1px solid #ffffff;
+  margin-top: 3vh;
+}
+div {
+  width: 100%;
+}
+
+* {
+  box-sizing: border-box;
+  padding-left: 5cm;
+}
+
+body {
+  background-color: rgb(20, 24, 28);
+  margin: 0%;
+}
+</style>
